2f880fc3d7a4f963d3595d1f090d9de46a458b7c
hyojung <hyojunghan1016@gmail.com>
2017-01-23 01:41:40 +0900

    Changed the css for the site.

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def post_list(request):
         return render(request, 'blog/post_list.html', {})
+def ex_css_1(request):
+        return render(request, 'css/ex_css_1.html', {})
