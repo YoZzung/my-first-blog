d06c7807697f723c913fcf70f6f6451dff22b713
hyojung <hyojunghan1016@gmail.com>
2017-01-23 15:23:52 +0900

    Changed the css for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^css/$', views.ex_css_1, name='ex_css_1'),
+    #url(r'^$', views.post_list, name='post_list'),
+    url(r'^css/ex1$', views.ex_css_1, name='ex_css_1'),
+    url(r'^css/ex2$', views.ex_css_2, name='ex_css_2'),
+    url(r'^css/ex3$', views.ex_css_3, name='ex_css_3'),
+    url(r'^$', views.index, name='index'),
+    url(r'^page_html/$', views.page_html, name='page_html'),
+    url(r'^page_vc/$', views.page_vc, name='page_vc'),
+    url(r'^page_op/$', views.page_op, name='page_op'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
-def post_list(request):
-        return render(request, 'blog/post_list.html', {})
+#def post_list(request):
+#        return render(request, 'blog/post_list.html', {})
 def ex_css_1(request):
         return render(request, 'css/ex_css_1.html', {})
+def ex_css_2(request):
+        return render(request, 'css/ex_css_2.html', {})
+def ex_css_3(request):
+        return render(request, 'css/ex_css_3.html', {})
+def index(request):
+        return render(request, 'blog/index.html', {})
+def page_html(request):
+        return render(request, 'blog/page_html.html', {})
+def page_op(request):
+        return render(request, 'blog/page_op.html', {})
+def page_vc(request):
+        return render(request, 'blog/page_vc.html', {})
